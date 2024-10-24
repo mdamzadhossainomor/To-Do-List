1a82198c09bc5001318129ca438fd00751105bfe
Md Amzad Hossain Omor <139708044+mdamzadhossainomor@users.noreply.github.com>
2024-10-24 17:51:33 +0600

    all id add js file

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let todoCount = 0;
+let totalTasks = 0;
+let completedTasks = 0;
+
+const todoInput = document.getElementById('todo-input');
+const addButton = document.getElementById('add-btn');
+const todoList = document.getElementById('todo-list');
+const summary = document.getElementById('summary');
