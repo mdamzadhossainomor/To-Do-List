c7dd4f2c2bd69eb555cee0864bbcf928ed514259
Md Amzad Hossain Omor <139708044+mdamzadhossainomor@users.noreply.github.com>
2024-10-24 18:03:27 +0600

    list item Date and  Month add

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
-
 let todoCount = 0;
 let totalTasks = 0;
 let completedTasks = 0;
 
-const todoInput = document.getElementById('todo-input');
-const addButton = document.getElementById('add-btn');
-const todoList = document.getElementById('todo-list');
-const summary = document.getElementById('summary');
+const todoInput = document.getElementById("todo-input");
+const addButton = document.getElementById("add-btn");
+const todoList = document.getElementById("todo-list");
+const summary = document.getElementById("summary");
 
-
-addButton.addEventListener('click', () => {
-    const taskText = todoInput.value;
-    if (taskText.trim() !== '') {
-      addTodoItem(taskText);
-      todoInput.value = '';
-    }
-  });
-  
-
-
-
-
-  function addTodoItem( ) {
-    
+addButton.addEventListener("click", () => {
+  const taskText = todoInput.value;
+  if (taskText.trim() !== "") {
+    addTodoItem(taskText);
+    todoInput.value = "";
   }
-  
- 
-  
+});
+
+function addTodoItem(taskText) {
+  const now = new Date();
+  const date = `${now.getDate()}/${now.getMonth() + 1}`;
+
+  const listItem = document.createElement("tr");
+}
